8a3bbe78fd764b796f72d0384df3f901307db69c
Aditia Darma Nasution <ad.nasution_pro@alphaphilindo.com>
2022-12-27 10:36:20 +0700

    update male

--- resources/views/home/berandaPage.blade.php
+++ resources/views/home/berandaPage.blade.php
@@ -6,7 +6,7 @@
     </div>
 <div id="map" style="height:600px;"></div>
 <div style="text-align: center;margin-top:20px;">
-    <a href="javascript:void(0)" class="btn">Cek lokasi UMKM terdekat</a><br/>
+    <a href="javascript:void(0)" class="btn" onclick="getLocation()">Cek lokasi UMKM terdekat</a><br/>
     <small>Harap aktifkan fitur lokasi(GPS) di smartphone anda untuk mendapatkan lokasi sekarang</small>
 </div>
 </div>
@@ -14,6 +14,7 @@
 
 <script>
     var map, infoWindow;
+
     function initMap() {
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: -6.173994159611038, lng: 106.7862738995646 },
@@ -32,5 +33,14 @@
             });
         @endforeach
     }
+
+    function getLocation()
+    {
+        let urlGetLocation = "https://www.googleapis.com/geolocation/v1/geolocate?key={{ env('GOOGLE_MAPS_API_KEY') }}";
+        $.post(urlGetLocation, function(data){
+            console.log(data);
+        });
+    }
+
     initMap();
 </script>
--- resources/views/home/homePage.blade.php
+++ resources/views/home/homePage.blade.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Aplikasi Klasifikasi Penyakit Kulit</title>
-  <!-- <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> -->
+  <title>Aplikasi lacak rute terdekat</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <!--Import materialize.css-->
   <link type="text/css" rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css" media="screen,projection" />
   <!--Let browser know website is optimized for mobile-->
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
   <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}" defer></script>
 </head>
